cc6c923a5444269d8fcfe3870c57a51bf00ab937
BigBrotherWatchingYou <99124106+BigBrotherWatchingYou@users.noreply.github.com>
2024-02-22 10:48:15 +0800

    aa

---
+++ ps1.py
@@ -0,0 +1,17 @@
+'''how this work:
+1. let user input information
+2. check information
+3. create information and store in category'''
+class create_information(self):
+    # get the user input
+    def create_name(self, name):
+        self.name = name
+    def create_phone(self, phone):
+        self.phone = phone
+    def create_email(self, email):
+        self.email = email
+
+class check_input(self):
+    #check if the input is correct (for example: name-alphabet only)
+    def check_name(self, name):
+        if self.
---
+++ test.py
@@ -0,0 +1,9 @@
+class B():
+    def xx(self, name):
+    name = input()
+    self.name = name
+    
+
+b = B()
+b.xx(fuckyou)
+print (self.name)
