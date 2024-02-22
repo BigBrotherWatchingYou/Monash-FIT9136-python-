0b20aebc9476c42488520fbe44841b576d4b2173
BigBrotherWatchingYou <99124106+BigBrotherWatchingYou@users.noreply.github.com>
2024-02-22 15:09:44 +0800

    f

--- Assignment1/ps1.py
+++ Assignment1/ps1.py
@@ -1,38 +1,20 @@
+#. Get user input function
+'''ask the user to input a corresponding value and return its input
+1. the input must match"requirements" or will be asked to input again, until 
+a valid one is inputted
+2. only valid result will return'''
 
-'''how this work:
-1. let user input info
-2. check info
-3. create info and store in category'''
-import string
 
+#. Encryption function
 
-class Create_info:
-    # create information
-    def __init__(self, name, phone, email):
-        self.name = name
-        self.phone = phone
-        self.email = email
-    def print_info(self):
-        print('yourname:' + self.name)
-        print('yourphone:' + self.phone)
-        print('youremail:' + self.email)
+#. Generate user id function
 
-def Input_info():
-    inputname = input('yourname')
-    inputphone = input('yourphone')
-    inputemail = input('youremail')
-    file = Create_info(inputname, inputphone, inputemail)
-    return inputname, inputphone, inputemail
-    
-def check_name(inputname):
-    if inputname.isalpha():
-        return inputname 
-    else: 
-        inputname = input("your name should be letters only, enter a new name")
-        return inputname
-def check_phone(inputphone):
-    if inputphone.isdigit():
-        return inputphone
-    else:
-        inputphone = input("your phone should be digit only, enter a new phone")
-        return inputphone
+#. Check username exist function
+
+#. Authenticate username and password function
+
+#. Add user to list function
+
+#. Test function
+
+#. Code encapsulation
