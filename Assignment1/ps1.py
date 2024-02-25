fbf58b56ac485fae7f2902aef72f2ceae89c3843
BigBrotherWatchingYou <99124106+BigBrotherWatchingYou@users.noreply.github.com>
2024-02-25 09:13:00 +0800

    aaa

--- Assignment1/ps1.py
+++ Assignment1/ps1.py
@@ -123,26 +123,26 @@ def generate_user_id(digitsnumber):
     print('your user name:' + ge_name)
     return ge_name
 
-user_name = generate_user_id(8)  
-user_name
+
+
 
 #.4. Check username exist function
-def check_username(n, old_list):
-    n = str(n)
+def check_username(username, old_list):
+    username = str(username)
     i = 0
     for k in old_list:
-        if n == k[0]: 
-            print('yes')
+        if username == k[0]: 
+            print('user exist')
             i = 1
     if i != 0:
         return True
     else:
         print("not in list")
         return False
-check_username(name, old_list)
+
 
 old_list = [["aa123", 2, 3, 4], ["b", 5, 6, 8], ["c", 5, 6, 8], ["d", 67, 6, 8]]    
-check_username(name, old_list)
+
 
 #.5. Authenticate username and password function
 '''1.please type your username
@@ -150,8 +150,43 @@ check_username(name, old_list)
 3. please enter your password
 4. check if password match
 5. give access and provide the information list  '''
+def authen_username_password(user_name, password):
+    user_name = str(user_name)
+    password = str(password)
+    if check_username(user_name, old_list) == True:
+        for m in old_list:
+            if user_name == m[0]:
+
+                if password == m[2]:
+                    print('password matched')
+                    return True
+                else:
+                    print("password incorrect")
+                    return False
+
+    else: 
+        print('unexist username')
+        return False
+    
+
+authen_username_password(username, password)
 #. Add user to list function
-
+def add_user(username, password, userid, old_list):
+    if check_username(username, old_list) == False:
+        return True
 #. Test function
+username = input("input your name")
+if check_username(username, old_list) == True:
+    authen_username_password(username, old_list)
+else:
+    print("user do not exist, create a new account?")
+    
+    choice = input("do you want to create a new account? enter y/n ")
+    if choice == "y":
+        
+        add_user(username, password, generate_user_id(5), old_list)
+    else:
+        
 
+get_input('letter', username)
 #. Code encapsulation
--- Assignment1/test3.py
+++ Assignment1/test3.py
@@ -1,17 +1,45 @@
-old_list = [["aa123", 2, 3, 4], ["b", 5, 6, 8], ["c", 5, 6, 8], ["d", 67, 6, 8]]
-name = input(" check username:")
-
-def check_username(n, old_list):
-    n = str(n)
+#.4. Check username exist function
+def check_username(username, old_list):
+    username = str(username)
     i = 0
     for k in old_list:
-        if n == k[0]: 
-            print('yes')
+        if username == k[0]: 
+            print('user exist')
             i = 1
     if i != 0:
         return True
     else:
         print("not in list")
         return False
-check_username(name, old_list)
 
+
+old_list = [["aa123", 2, 3, 4], ["b", 5, 6, 8], ["c", 5, 6, 8], ["d", 67, 6, 8]]    
+
+
+#.5. Authenticate username and password function
+'''1.please type your username
+2. check if username exist
+3. please enter your password
+4. check if password match
+5. give access and provide the information list  '''
+def authen_username_password(user_name, password):
+    user_name = str(user_name)
+    password = str(password)
+    if check_username(user_name, old_list) == True:
+        for m in old_list:
+            if user_name == m[0]:
+
+                if password == m[2]:
+                    print('password matched')
+                    return True
+                else:
+                    print("password incorrect")
+                    return False
+
+    else: 
+        print('unexist username')
+        return False
+    
+username = input('username:')
+password = input('type passwrod:')
+authen_username_password(username, password)
