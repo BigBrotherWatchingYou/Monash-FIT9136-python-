87415c90bb657ad1ef280a62510401832ac2d0f2
BigBrotherWatchingYou <99124106+BigBrotherWatchingYou@users.noreply.github.com>
2024-02-26 11:33:24 +0800

    rewrite

---
+++ Assignment1/ps1_new.py
@@ -0,0 +1,22 @@
+user_id_list = []
+class User_Management_System(self, user_id_list):
+    def __init__(self, username, userid, password, email):
+        self.list = {}
+        self.list[userid] = {self.username, self.userid, self.password, self.email}
+        
+    def add_user_to_user_id_list(userid, user_id_list):
+        user_id_list.append(userid)
+    def add_user(self, username, userid, password, email):
+        self.list[userid] = {"username": username, "userid": userid, "userpassword": password, "useremail": email}
+
+    def check_user(userid):
+        if userid in user_id_list:
+            print(f"user{userid}exists")
+        else:
+            print(f"user{userid}does not exists")
+    def update_user(self, userid):
+
+
+
+def run(self):
+
--- Assignment1/test3.py
+++ Assignment1/test3.py
@@ -1,6 +1,2 @@
-def add_user(user_dict, username, password, userid):
-    
-    user_dict[username] = {"username": username, "password": password, "userid": userid}
-    return user_dict[username]
-data = {}
-add_user(data, "Yname", "awef234", "234234")
+userid = "3r23"
+print(f"userid{userid}")
