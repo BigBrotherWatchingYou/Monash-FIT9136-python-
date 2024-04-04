f878e2da7f9ce6c6cbe54a336fd860d3b1568903
BigBrotherWatchingYou <woshenyibande@gmail.com>
2024-04-04 20:58:03 +0800

    test

--- Assignment2/test1.py
+++ Assignment2/test1.py
@@ -1,16 +1,3 @@
-admin_list = ["admin","admin2","admin3"]
-student_list = ["student", "student2", "student3" , "student4"]
-instructor_list = ["instructor", "instructor2", "instructor3" , "instructor4", "instructor5"]
-k = "student"
-i = 0
-def w(i):
-    if i >= 3:
-        quit
-    print("w")
-    i += 1
-    k1()
-    
-def k1():
-    print("k")
-    return
-w(i)
+class SSS:
+    def wef():
+        print("aa")
--- Assignment2/test2.py
+++ Assignment2/test2.py
@@ -1,11 +1,2 @@
-import requests
-url = "https://github.com/BigBrotherWatchingYou/Monash-FIT9136-python-/blob/main/Assignment2/raw_data.txt"
-read_file = requests.get(url)
-if read_file.status_code == 200:
-    # Get the file contents from the read_file
-    file_contents = read_file.text
-
-    # Print the contents
-    print(file_contents)
-else:
-    print("Failed to fetch file:", read_file.status_code)
+from test1 import SSS
+SSS.wef()
