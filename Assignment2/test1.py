a8a84d940a250c403a3db2e1e4a9db2ffc3d4137
BigBrotherWatchingYou <woshenyibande@gmail.com>
2024-04-05 17:06:45 +0800

    read_test

--- Assignment2/test1.py
+++ Assignment2/test1.py
@@ -1,3 +1,15 @@
-class SSS:
-    def wef():
-        print("aa")
+import os
+
+file_path = "course_file.txt"
+
+# Check if the file exists
+if os.path.exists(file_path):
+    # Open the text file in read mode
+    with open(file_path, 'r') as file:
+        # Read the contents of the file
+        file_contents = file.read()
+
+        # Print the contents
+        print(file_contents)
+else:
+    print(f"The file '{file_path}' does not exist.")
