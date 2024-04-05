9dd71f37441c68e92b17ec5e86d1e1ffa62b0eb4
BigBrotherWatchingYou <woshenyibande@gmail.com>
2024-04-05 17:03:51 +0800

    able to read file

--- Assignment2/Course.py
+++ Assignment2/Course.py
@@ -1,25 +1,49 @@
 class Course:
     def constructor():
-'''The attributes of course are course_id(int, default value -1), course_title(str, default
+        pass
+    '''The attributes of course are course_id(int, default value -1), course_title(str, default
 value “”), course_image_100x100(str, default value “”), course_headline(str, default
 
 value “”), course_num_subscribers(int, default value -1), course_avg_rating(float,
 default value -1.0), course_content_length(float, default value -1.).'''
     def find_course_by_title_keyword(keyword):
-'''This method has a positional argument keyword(str). Based on the given keyword, it
+        pass
+    '''This method has a positional argument keyword(str). Based on the given keyword, it
 searches the course title of all courses in the course.txt file to find the result. All the
 result courses will be created as a course object and added into a result list. Finally,
 return the result list. The result list looks like [Course(), Course(), Course()….]. If not
 found, return an empty list.'''
     def find_course_by_id(course_id):
-'''This method has a positional argument course id(int or str). You are required to
+        pass
+    '''This method has a positional argument course id(int or str). You are required to
 search for the course according to the course id. A course object will be returned. If
 not found, return None.'''
     def find_course_by_instructor_id(instructor_id):
-'''This method has a positional argument instructor id(int or str). Based on the
+        pass
+    '''This method has a positional argument instructor id(int or str). Based on the
 instructor id, a list of course objects will be generated and returned. The result list
 looks like [Course(), Course(), Course()….]. If not found, return an empty list.'''
     def courses_overview():
-'''This method returns a string that shows the total number of courses.'''
+        pass
+    '''This method returns a string that shows the total number of courses.'''
     def __str__():
-'''Return a string containing all course information.'''
+        pass
+    '''Return a string containing all course information.'''
+
+
+
+course = "course_id;;;course_title;;;image_100x100;;;   ;;;num_of_\
+subscribers;;;avg_rating;;;course_content_length”.\
+For each line in the raw_data.txt file, you can copy and paste it to Json Parser Online\
+to check the format. Each line contains more than one course. All the corresponding\
+attributes can be found in the text. You can use the library re or str methods to\
+extract the data. The course content length in the text is like “40.5 hours”. Only 40.5\
+need to be retrieved. The course data will be saved into the course.txt file."
+
+review = "“review_id;;;review_content;;;review_rating;;;course_id”\
+The course id can be obtained from each file's name in the review_data folder.\
+'''"
+
+
+
+   
--- Assignment2/User.py
+++ Assignment2/User.py
@@ -1,17 +1,59 @@
+import random
 class User:
 # User class handles the fundamental methods of all users. The User class is the parent class of Admin, Instructor and Student classes.
     def constructor():
+        pass
 #A user must have id(int, default value -1), username(str, default value “”),
 #password(str, default value “”).
     def generate_unique_user_id():
+        pass
 #This method checks the files user_admin.txt, user_instructor.txt and user_student.txt
 #to generate an unique user id. The return result is a 10 digits integer.
     def encryption(input_password):
+        pass
 #This method encrypts the input_password to a string that is difficult to read by
 #humans. Reuse the encryption algorithm that was in A1 to encode the input
 #password. The encrypted password will be returned and the type is string.
-    def login():
-'''Each user can call the login method to perform authentication. In this login() method,
+    def login():   
+        # Fig1 show menu example
+        # return "Admin login" or "User login"
+        
+        # let user input
+        userinput = input("format: username password(enter 'exit' to quit)")
+        
+        #quit
+        if userinput == "exit":
+            print("Thanks for using, programm quit")
+            quit
+            
+        # incorrect input
+        if len(userinput).split() != 2:
+            print("format(there should be a space between name and password): username password")
+            return Main.login()
+        else:
+            # legal input
+            username = userinput.split()[0]
+            password = userinput.split()[1]
+            
+        # start authenticating username and password    
+        authenticate_user(username, password)
+            
+        def authenticate_user(username, password):
+            #check do the user exist
+            if username not in (admin_list and  instructor_list and  student_list):
+                #user do not exist
+                print("user id do not exist")
+                return Main.login()
+            if userdata[username]["password"] == password:
+                # login successful
+                return username
+            else:
+                # login failed
+                print("incorrect username and password\n\
+                    input format:username password")
+                return User.login()
+            
+    '''Each user can call the login method to perform authentication. In this login() method,
 it is required to call the encryption() method defined before to encode the password.
 The encoded password can be used to compare with the password extracted from
 files. You are required to to check the user_admin.txt, user_instructor.txt and
@@ -21,27 +63,24 @@ only be “Admin”, “Instructor”, “Student”), login_user_info(any type 
 or str, this value can be used to create different types of user object (Admin, Student
 or Instructor)).'''
     def extract_info():
+        pass
 #This method prints out a message “You have no permission to extract information”.
     def view_courses(args=[]):
+        pass
 #This method prints out a message “You have no permission to view courses”.
     def view_users():
+        pass
 #This method prints out a message “You have no permission to view users”.
     def view_reviews(args=[]):
+        pass
 #This method prints out a message “You have no permission to view reviews”.
     def remove_data():
+        pass
 #This method prints out a message “You have no permission to remove data”.
     def __str__():
+        pass
 #This method returns a formatted user string: “user_id;;;username;;;password”. All the
 #attributes are concatenated using “;;;”'''
 
 
 
-
-
-
-
-
-
-
-
-   
