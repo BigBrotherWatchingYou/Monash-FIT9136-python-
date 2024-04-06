a878ec808ae834dc969e2936c624ae3de2f97387
BigBrotherWatchingYou <woshenyibande@gmail.com>
2024-04-06 16:59:10 +0800

    admin_register done

--- Assignment2/Admin.py
+++ Assignment2/Admin.py
@@ -11,17 +11,62 @@ class Admin(User):
     #and password(str, default value “”) which can be inherited from the parent class.
     
     def register_admin():
-        
-        
-        # create admin
-        userdata[username] = {"username":username , "password":password, "role": "admin"}           
-                
-            #quit
+        print("welcome to register, your role is: admin")
+        # open and read admin.txt
+        admin_file = open("Assignment2/user_admin.txt", 'r')
+        admin_list = admin_file.read().split()
+        admin_file.close()
+     
+        # create username and password
+        def input_username():
+            print("Enter your name")
+            get_username = input("Enter 'quit' to quit")
+            if get_username == "quit":
+                quit
+            elif get_username in admin_list:
+                print("username already exits")
+                return input_username()
+            else:
+                print("valid username")
+                return get_username
+
+        def input_password():
+            print("enter your password")
+            get_password = input("enter password")
+            check_password = input("enter password again")
+                # double check password
+            if get_password != check_password:
+                print("both password should be the same")
+                return input_password()
+            else:
+                return get_password
             
+        
+        username = input_username()
+        password = input_password()
+        role = "admin"
+        # create admin
+        admin ={}
+        admin[username] = {"username":username , "password":password, "role": "admin"}
+        
+        def create_admin(username, password, role):
+            # read user_admin.txt
+            with open("Assignment2/user_admin.txt", 'r') as file:
+                file_content = file.read()
+            
+            file_content += '\n' + admin[username]
+            # write admin into file
+            with open("Assignment2/user_admin.txt","w") as file:
+                file.write(file_content)
+        
+            
+            print("admin created successfully")    
+            
+            
+        create_admin(username, password, role)    
 
             
-        def create_admin():
-            pass    
+         
             
     #This method checks the user_admin.txt file to find out whether the username already
         #exists or not. If not, register this admin. If it exists, do nothing.
