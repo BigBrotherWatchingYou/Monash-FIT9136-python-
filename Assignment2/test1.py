3a0523f92bde5392c01900eea147442e0df41f20
BigBrotherWatchingYou <woshenyibande@gmail.com>
2024-04-06 10:11:10 +0800

    write successful

--- Assignment2/test1.py
+++ Assignment2/test1.py
@@ -1,9 +1,21 @@
 import os
+def append_to_txt_file(file_name, text_to_append):
+    try:
+        # Open the file in read mode
+        with open(file_name, 'r') as file:
+            file_contents = file.read()
 
-cour_file = open("Assignment2/user_admin.txt",'r' )
-cour_list = cour_file.read().split()
-file = []
-cour_file.close()
+        # Append the new text
+        file_contents += '\n' + text_to_append
 
-for k in cour_list:
-    print(k)
+        # Open the file in write mode and overwrite the existing content
+        with open(file_name, 'w') as file:
+            file.write(file_contents)
+        
+        print("Text appended successfully to", file_name)
+    
+    except FileNotFoundError:
+        print("Error: File not found.")
+
+# Example usage:
+append_to_txt_file("Assignment2/user_admin.txt", 'This is new text to append.')
