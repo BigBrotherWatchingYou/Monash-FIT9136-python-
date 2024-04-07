40b3fd17831f0addd34ac55bd203d87404821987
BigBrotherWatchingYou <woshenyibande@gmail.com>
2024-04-07 09:00:21 +0800

    testing

--- Assignment2/test2.py
+++ Assignment2/test2.py
@@ -1,4 +1,16 @@
-
-with open("Assignment2/user_admin.txt") as f:
-    file = [item.strip() for item in f]
-    print(file)
+try:
+    with open("Assignment2/user_admin.txt") as f:
+        admin_data = (k.strip() for k in f)
+except FileExistsError:
+    admin_data = []
+    
+while True:
+    # loop
+    username = input(("Enter your name (or 'quit' to quit): ").strip())
+    if username.lower() == "quit":
+        break
+    
+    if any (username in names for names in admin_data ):
+        # username exists
+        print("Username already exists. Please choose a different username.")
+        
