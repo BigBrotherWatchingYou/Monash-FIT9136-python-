9cd9f425dd27f1554b08154e8467f1c4547a4bb2
BigBrotherWatchingYou <woshenyibande@gmail.com>
2024-04-08 18:59:07 +0800

    learning about "dictionary"

--- Assignment2/test2.py
+++ Assignment2/test2.py
@@ -1,16 +1,18 @@
+import os
 try:
     with open("Assignment2/user_admin.txt") as f:
-        admin_data = (k.strip() for k in f)
+        admin_data = [k.strip() for k in f]
 except FileExistsError:
     admin_data = []
-    
+print(admin_data)    
 while True:
     # loop
     username = input(("Enter your name (or 'quit' to quit): ").strip())
     if username.lower() == "quit":
         break
     
-    if any (username in names for names in admin_data ):
+    if any (username in  admin_data ):
         # username exists
         print("Username already exists. Please choose a different username.")
+        continue
         
