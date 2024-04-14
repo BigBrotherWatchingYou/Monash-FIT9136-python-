66368efe669ddda89b031a2cb6a9ac72d75a80de
BigBrotherWatchingYou <woshenyibande@gmail.com>
2024-04-14 15:46:29 +0800

    test2

--- Assignment2/test2.py
+++ Assignment2/test2.py
@@ -1,12 +1,14 @@
-import os
-try:
-    with open("Assignment2/course_file.txt",'r') as c:
-        course_file = [t.strip().split(";;;") for t in c.readlines() if t.strip()]
-        total_number = len(course_file)
-                
+def courses_overview():
+        
+    try:
+        with open("Assignment2/course_file.txt",'r') as c:
+            course_file = [t.strip().split(";;;") for t in c.readlines() if t.strip()]
+            total_number = len(course_file)                
                     
                 
-except FileNotFoundError:
-    pass
+    except FileNotFoundError:
+        pass
         
-print(total_number)
+    return total_number
+
+print(courses_overview())
