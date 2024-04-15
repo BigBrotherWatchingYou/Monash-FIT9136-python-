779100af49d0b7392ba4649415f1b4a51813e421
BigBrotherWatchingYou <woshenyibande@gmail.com>
2024-04-15 10:14:45 +0800

    init

--- chess/Main.py
+++ chess/Main.py
@@ -3,6 +3,10 @@ import pygame
 
 # part 1 : set up game loops and variable images
 pygame.init()
+pygame.display.set_mode([800,800])
+font = pygame.font.Font('freesansbold', 20)
+timer = pygame.time.Clock(100)
+fps = 60
 
 # part 2 : main game loop
     # event handling
