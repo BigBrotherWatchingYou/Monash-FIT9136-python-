cae6ffb1c75336c47f85d565da3cb8824cc24283
BigBrotherWatchingYou <woshenyibande@gmail.com>
2024-04-15 10:29:23 +0800

    images upload

--- chess/Main.py
+++ chess/Main.py
@@ -4,9 +4,15 @@ import pygame
 # part 1 : set up game loops and variable images
 pygame.init()
 pygame.display.set_mode([800,800])
-font = pygame.font.Font('freesansbold', 20)
+font = pygame.font.Font('freesansbold.ttf', 20)
+pygame.display.set_caption('Chess game')
 timer = pygame.time.Clock(100)
 fps = 60
+  # game variables and images
 
 # part 2 : main game loop
     # event handling
+run = True
+while run:
+    timer.tick(fps)
+    screen.fill('black')
