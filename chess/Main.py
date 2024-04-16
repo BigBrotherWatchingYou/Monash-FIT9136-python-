e41c84b5600d88b522d055499ce8172ed835df45
BigBrotherWatchingYou <woshenyibande@gmail.com>
2024-04-16 18:40:34 +0800

    chess display

--- chess/Main.py
+++ chess/Main.py
@@ -13,8 +13,18 @@ white_pieces = ['rook','knight','bishop','king','queen','bishop','knight','rook'
                 'pawn','pawn','pawn','pawn','pawn','pawn','pawn']  
 white_locations =[(0,0),(1,0),(2,0),(3,0),(4,0),(5,0),(6,0),(7,0),
                   (0,1),(1,1),(2,1),(3,1),(4,1),(5,1),(6,1),(7,1)]
+
+black_pieces = ['rook','knight','bishop','king','queen','bishop','knight','rook',
+                'pawn','pawn','pawn','pawn','pawn','pawn','pawn']
 black_locations = [(0,7),(1,7),(2,7),(3,7),(4,7),(5,7),(6,7),(7,7),
                   (0,6),(1,6),(2,6),(3,6),(4,6),(5,6),(6,6),(7,6)]
+captured_piece_white = []
+captured_piece_black = []
+
+#  white turn: no selection: 1-whites turn piece selected; 2- black turn piece selected'
+turn_step = 0
+selection = 100
+
 
 # part 2 : main game loop
     # event handling
@@ -22,3 +32,11 @@ run = True
 while run:
     timer.tick(fps)
     screen.fill('black')
+    
+    # event handling
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        run = False
+    
+    pygame.display.flip()
+pygame.quit()
