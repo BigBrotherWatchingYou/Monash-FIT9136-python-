1b72881cff9a9b67d8deb52634a5e0d2547c6dad
Reinhard Zeng <woshenyibande@gmail.com>
2024-07-31 21:41:48 +1000

    task7

---
+++ Week1-4Assignment1/task7.py
@@ -0,0 +1,23 @@
+your_variable_name = \
+["abcdefghijklm",
+ "nopqrstuvwxyz"]
+
+
+key = str(input("Enter a string to type: "))
+# signal for stopping the loop should base on the count on the key
+stop_signal = len(key)
+operation = ""
+
+print(stop_signal)
+
+
+    
+    def 
+while stop_signal<=0:
+    
+    # Everytime it press the key, stop_signal -=1
+    # and operation add a 'p'
+    operation += "p"
+    stop_signal -= 1 
+
+print("The robot must perform the following operations:\n")
