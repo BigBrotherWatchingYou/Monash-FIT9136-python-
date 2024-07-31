4b111f995688423381eeee664c19a281adfdef78
Reinhard Zeng <woshenyibande@gmail.com>
2024-07-31 22:28:36 +1000

    30%done task7

--- Week1-4Assignment1/task7.py
+++ Week1-4Assignment1/task7.py
@@ -1,23 +1,56 @@
-your_variable_name = \
-["abcdefghijklm",
- "nopqrstuvwxyz"]
+your_variable_name = ["abcdefghijklm", "nopqrstuvwxyz"]
 
 
-key = str(input("Enter a string to type: "))
+string = str(input("Enter a string to type: "))
 # signal for stopping the loop should base on the count on the key
-stop_signal = len(key)
+# stop_signal = len(string)
 operation = ""
 
-print(stop_signal)
 
-
-    
-    def 
-while stop_signal<=0:
-    
+def goes_to_the_key(input_key, operation):
     # Everytime it press the key, stop_signal -=1
-    # and operation add a 'p'
-    operation += "p"
-    stop_signal -= 1 
+    operation += 'p'
+    return operation
 
-print("The robot must perform the following operations:\n")
+def search_key(input_key, dictionary):
+    return_value = 
+    #check if it is of the first line
+    for items in input_key:
+        if items in dictionary[0]:
+        #check position of the key in the line
+            return dictionary[0][items]
+        elif input_key in dictionary[1]:
+        
+
+
+def check_valid(input_key, dictionary):
+    return_value = True
+    for item in input_key:
+        if item not in dictionary[0] and item not in dictionary[1]:
+            return_value = False
+    return return_value
+
+
+
+
+
+# main programm
+if check_valid(string, your_variable_name) == False:
+    # invalid key
+    print("The string cannot be typed out.")
+
+else:
+    # the key is valid
+    while len(string) > 0:
+        current_key = string[0]
+        new_key = string[1]
+        current_location = search_key(current_key)
+        new_key_location = search_key(new_key)
+
+        operation += goes_to_the_key(current_key_location, new_key_location)
+        # remove the first character of string
+        string = string[1:]
+        
+    
+
+print("The robot must perform the following operations:\n"+ operation)
--- Week1-4Assignment1/test.py
+++ Week1-4Assignment1/test.py
@@ -1,27 +1,6 @@
 operation = "pla00"
 check_list = ["abcdefghijklm",
  "nopqrstuvwxyz"]
+operation = operation[1:]
 
-
-def check_key_valid(input_key, checking_list):
-    # check if the key is valid
-    for items in input_key:
-        print(items)
-        if items in checking_list[0]:
-            print("list1")
-        elif items in checking_list[1]:
-            print('list2')    
-        else:
-            print("invalid")
-
-def check_valid(input_key, dictionary):
-    return_value = True
-    for item in input_key:
-        if item not in dictionary[0] and item not in dictionary[1]:
-            return_value  = False
-        
-    return return_value
-        
-print(check_valid("pla00", check_list))
-
-# check_key_valid(operation, check_list)
+print("awef\n" + "wefwe")
